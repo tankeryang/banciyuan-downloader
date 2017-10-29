daaa5b22aff27c55491e3119225932ddbc0efa8d
tankeryang <youngzyang@outlook.com>
2017-10-29 22:43:38 +0800

    fix: normalize

--- download.py
+++ download.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     banciyuan_home_dir = input('input banciyuan home path (i.e. E:\\banciyuan): ')
     coser_dir = banciyuan_home_dir + '\\' + create_user_folder_name(coser_id)
 
-	if not os.path.exists(coser_dir):
-		os.mkdir(coser_dir)
-		get_pics(account, password, coser_id, coser_dir)
-	else:
-		count = len(os.listdir(coser_dir))
-		get_pics(account, password, coser_id, coser_dir, count)
+    if not os.path.exists(coser_dir):
+        os.mkdir(coser_dir)
+        get_pics(account, password, coser_id, coser_dir)
+    else:
+        count = len(os.listdir(coser_dir))
+        get_pics(account, password, coser_id, coser_dir, count)
