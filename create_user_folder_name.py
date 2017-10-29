4ede8fb9ead1506e6dd3f076d9b8b3826e84e452
tankeryang <youngzyang@outlook.com>
2017-10-29 22:45:44 +0800

    fix: normalize

--- create_user_folder_name.py
+++ create_user_folder_name.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-headers  = {
+headers = {
     'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.1144',
     'Connection' : 'keep-alive',
     'Referer' : 'http://bcy.net'
