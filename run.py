f3e5d08293bde89dbe7fb49d39c561e40b5ab323
tankeryang <youngzyang@outlook.com>
2018-06-18 23:00:34 +0800

    fix: 多线程创建文件夹冲突

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from bcy_downloader import Downloader
 
 if __name__ == '__main__':
-    # account = input('Enter your banciyuan account (phone number/e-mail): ')
-    # password = input('Enter your banciyuan password: ')
-    # coser_id = input('Enter banciyuan coser id: ')
-    # bcy_home_dir = input('Enter banciyuan home path (i.e. E:\\banciyuan): ')
+    account = input('Enter your banciyuan account (phone number/e-mail): ')
+    password = input('Enter your banciyuan password: ')
+    coser_id = input('Enter banciyuan coser id: ')
+    bcy_home_dir = input('Enter banciyuan home path (i.e. E:\\banciyuan): ')
 
-    dl = Downloader(account='18926229838', password='banciyuan950820', coser_id='1016773', bcy_home_dir='/Users/yang/Pictures/banciyuan')
+    dl = Downloader(account=account, password=password, coser_id=coser_id, bcy_home_dir=bcy_home_dir)
     dl.run()
