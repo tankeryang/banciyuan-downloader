19eb8730e6cf942650bfe579c027779fbfcb2303
tankeryang <youngzyang@outlook.com>
2018-06-25 22:05:25 +0800

    fix: conflict

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     account = input('Enter your banciyuan account (phone number/e-mail): ')
     password = input('Enter your banciyuan password: ')
     coser_id = input('Enter banciyuan coser id: ')
-    bcy_home_dir = input('Enter banciyuan home path (i.e. E:\\banciyuan): ')
+    bcy_home_dir = input('Enter banciyuan home path (i.e. E:/banciyuan): ')
 
     dl = Downloader(account=account, password=password, coser_id=coser_id, bcy_home_dir=bcy_home_dir)
     dl.run()
