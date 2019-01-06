5e4d5b7175e10c386806c5c8c7e80cfb47f31d8d
tankeryang <youngzyang@outlook.com>
2019-01-06 19:42:23 +0800

    mod: bcy_downloader

--- bcy_downloader.py
+++ bcy_downloader.py
@@ -301,7 +301,7 @@ if __name__ == "__main__":
     # account = input('Enter your banciyuan account (phone number/e-mail): ')
     # password = input('Enter your banciyuan password: ')
     coser_id = input('Enter banciyuan coser id: ')
-    bcy_home_dir = input('Enter banciyuan home path (i.e. [WINDOWS]E:/banciyuan | [MAC/LINUX]/home/username/Picture/banciyuan): ')
+    bcy_home_dir = input('Enter banciyuan home path (i.e. [WINDOWS]E:\\banciyuan | [MAC/LINUX]/home/username/Picture/banciyuan): ')
 
     dl = Downloader(coser_id=coser_id, bcy_home_dir=bcy_home_dir)
     dl.run()
